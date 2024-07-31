6ca5547e0a085e57a6cfec6c16254d3d7b941f75
Harshit Borana <160480741+harshit075@users.noreply.github.com>
2024-07-31 14:19:59 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+//     // int a = 123;
+//     // cout<<a<< '\n';
+
+//     char b = 'v';
+//     cout<<b <<"\n";
+
+//     bool bl = true;
+//     cout<< bl << endl;
+
+//     float F = 1.2;
+//     cout<< F << endl;
+
+//     double D = 1.23;
+//     cout << D << endl;
+
+//     // int size = sizeof(a);
+//     // cout <<"Size of a is "<< size << endl;
+
+//     int a = 'a';
+//     cout << a << '\n';
+// 
+// unsigned int a = -112;
+// cout<< a << endl;
+
+int a = 2;
+int b = 3;
+
+bool first = (a=b);
+cout << first;
+
+bool second = (a>b);
+cout << second;
+
+bool third = (a<b);
+cout << third;
+
+bool fourth = (a<b);
+cout << fourth;
+
+bool fifth = (a>>b);
+cout << fifth;
+
+bool sixth = (a<<b);
+cout << sixth;
+
+
+}
+
