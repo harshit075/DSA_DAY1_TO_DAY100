52b15656993fcd443a01d305a9286aeca536c4f8
Harshit Borana <160480741+harshit075@users.noreply.github.com>
2024-08-02 12:28:16 +0530

    Add files via upload

---
+++ Conditions.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+//    int a, b;
+
+//    cout << "Enter the value of a: "<<endl;
+//    cin >>a;
+//    cout << "Enter the value of b: "<<endl;
+//    cin >>b;
+   
+//    if ( a > b){
+//     cout << "a is greater than b";
+//    }
+//    else {
+//     cout << "b is greater than a";
+//    }
+    // int a;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+
+    // if (a>0){
+    //     cout<<"a is positive";
+    // }
+    // else {
+    //     if(a<0){
+    //         cout<< "a is negative";
+    //     }
+    //     else {
+    //         cout << " a is 0";
+    //     }
+    // }
+
+    // int a = 2;
+    // int b = a+1;
+
+    // if ((a=3)==b){
+    //     cout<<a;
+    // }
+    // else{
+    //     cout<<a+1;
+    // }
+
+    // int a = 20;
+
+    // if (a > 20) {
+    //     cout << "love";
+    // }
+    // else if (a == 20){
+    //     cout << "Medical";
+    // }
+    // else {
+    //     cout << "Harshit";
+    // }
+    // cout<< a; 
+
+//    int n;
+
+//    cin >> n;
+
+//    int i = 1;
+//    int sum = 0;
+
+//    while (i<=n) {
+//     sum = sum + i;
+    
+//     i = i + 1;
+//    }
+
+//    cout << " value of sum is " << sum << endl;
+
+
+   
+   
+
+
+}
